b7a228d62410297b063d5898afa6a402fab18729
Nartuom <thomas.burton.lawl@gmail.com>
2020-02-11 10:50:55 +0000

    add node and file structure and complete basic html/css structure for homepage.

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express"),
+    app     = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index");
+});
+
+var url = process.env.DATABASEURL;
+app.listen(process.env.PORT||3000, process.env.IP, function(){
+	console.log("Server Live at " + url);
+});
