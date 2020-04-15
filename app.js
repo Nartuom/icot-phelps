6271cf9727e03bc5951f9c540c8160e8f8c94479
Thomas Burton-Lawl <45258877+Nartuom@users.noreply.github.com>
2020-04-15 22:39:27 +0100

    temp error page fix

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get("*", function(req, res, next){
     let err = new Error(`${req.ip} tried to reach ${req.originalUrl}`);
     err.statusCode = 404;
     next(err);
-    res.redirect("/error");
+    res.redirect("/home");
 });
 
 //POST route form contact form
