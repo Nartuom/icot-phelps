5fbab4c8ff98f6b9dc812d2097cee21c5b2c5426
Nartuom <thomas.burton.lawl@gmail.com>
2020-08-03 14:19:15 +0100

    add animation to navbar burger

--- public/scripts/ocot.js
+++ public/scripts/ocot.js
@@ -2,8 +2,12 @@ const toggleButton = document.getElementsByClassName("toggle-button")[0];
 const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 const navbar = document.getElementsByClassName("navbar");
 const navbarLi = [...document.getElementsByClassName("navbarLi")];
+const burgerBar = document.getElementsByClassName("bar");
 
 toggleButton.addEventListener("click", () => {
+    [...burgerBar].forEach(function(bar){
+        bar.classList.toggle("onClick");
+    })
     navbarLi.forEach(function(li){
         li.classList.toggle("activeLi");
     });
